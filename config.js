fa63efb57365ee799ffb2f36d6dd344a04b916b4
obrendonadriano <brendonbolf1@outlook.com>
2026-03-22 10:53:18 -0300

    Configure Supabase project connection

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ window.APP_CONFIG = {
     message: "Olá, meu nome é {name}. Acabei de preencher o cadastro da Dilson Stein, tenho {age} anos e moro em {city}. Gostaria de marcar meu horário para finalizar meu atendimento."
   },
   supabase: {
-    url: "",
-    anonKey: "",
+    url: "https://grohztewndqwfyvdgjar.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdyb2h6dGV3bmRxd2Z5dmRnamFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxNTkyODQsImV4cCI6MjA4OTczNTI4NH0.EsY9wxVaocBOAx541rF200nNWJBT30aiP7Rk2iJX0LM",
     table: "leads"
   },
   crm: {
